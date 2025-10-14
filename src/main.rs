6109a1c59355b3fb3285a4b3313e8bb03db0bff3
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-10-14 21:58:34 +0800

    chore: init cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-  println!("Hello, world!");
+mod cli;
+use cli::{Cli, Commands};
+
+use clap::Parser;
+use human_panic::{setup_panic, metadata};
+
+#[tokio::main]
+async fn main() {
+  setup_panic!(metadata!().homepage("https://github.com/moeru-ai/ortts/issues"));
+
+  let cli = Cli::parse();
+
+  if let Some(command) = cli.command {
+    match command {
+      Commands::Serve(_) => todo!(),
+      Commands::Run => todo!(),
+    }
+  } else {
+    todo!()
+  }
 }
