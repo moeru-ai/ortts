15ae87a933898e4b8faab5c7a4af03f8568289cc
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-10-15 11:34:44 +0800

    feat: openapi

--- crates/server/src/lib.rs
+++ crates/server/src/lib.rs
@@ -1,7 +1,22 @@
 use axum::Router;
+use utoipa::OpenApi;
+use utoipa_axum::routes as route;
+use utoipa_axum::router::OpenApiRouter;
 
 mod routes;
+mod openapi;
+
+use openapi::ApiDoc;
+use utoipa_scalar::{Scalar, Servable};
 
 pub fn new() -> Router {
-  Router::new().fallback(routes::not_found)
+  let (router, api) = OpenApiRouter::with_openapi(ApiDoc::openapi())
+    .routes(route!(routes::openapi_json::openapi_json))
+    .split_for_parts();
+
+  let router = router
+    .merge(Scalar::with_url("/", api))
+    .fallback(routes::not_found);
+
+  router
 }
--- crates/server/src/routes/mod.rs
+++ crates/server/src/routes/mod.rs
@@ -1,2 +1,4 @@
 mod not_found;
 pub use not_found::not_found;
+
+pub mod openapi_json;
