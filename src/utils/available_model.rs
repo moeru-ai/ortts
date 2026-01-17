6cd2728aa0a03f0a7ef9b27467dd08801c261663
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-01-17 17:01:46 +0800

    refactor: use strum

--- src/utils/available_model.rs
+++ src/utils/available_model.rs
@@ -1,36 +1,29 @@
+use std::str::FromStr;
+use strum::{Display, EnumIter, EnumProperty, EnumString, IntoEnumIterator, IntoStaticStr};
+
+#[derive(Debug, PartialEq, EnumProperty, EnumString, EnumIter, Display, IntoStaticStr)]
+#[strum(serialize_all = "kebab-case")]
 pub enum AvailableModel {
+  #[strum(props(hf = "onnx-community/chatterbox-multilingual-ONNX"))]
   ChatterboxMultilingual,
+  #[strum(props(hf = "onnx-community/Kokoro-82M-v1.0-ONNX"))]
   Kokoro,
 }
 
 impl AvailableModel {
   pub fn model_name(&self) -> &str {
-    match self {
-      Self::ChatterboxMultilingual => "chatterbox-multilingual",
-      Self::Kokoro => "kokoro",
-    }
+    self.into()
   }
 
   pub fn hf_id(&self) -> &str {
-    match self {
-      Self::ChatterboxMultilingual => "onnx-community/chatterbox-multilingual-ONNX",
-      Self::Kokoro => "onnx-community/Kokoro-82M-v1.0-ONNX",
-    }
+    self.get_str("hf").unwrap_or_default()
   }
 
   pub fn from_model_name(model_id: &str) -> Option<Self> {
-    match model_id {
-      "chatterbox-multilingual" => Some(Self::ChatterboxMultilingual),
-      "kokoro" => Some(Self::Kokoro),
-      _ => None,
-    }
+    Self::from_str(model_id).ok()
   }
 
   pub fn from_hf_id(hf_id: &str) -> Option<Self> {
-    match hf_id {
-      "onnx-community/chatterbox-multilingual-ONNX" => Some(Self::ChatterboxMultilingual),
-      "onnx-community/Kokoro-82M-v1.0-ONNX" => Some(Self::Kokoro),
-      _ => None,
-    }
+    Self::iter().find(|variant| variant.hf_id() == hf_id)
   }
 }
