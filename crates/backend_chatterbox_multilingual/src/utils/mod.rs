baf2aeb13b45d1b65e9a4ec8606d5b70bb251f1e
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-01-21 13:39:18 +0800

    refactor: split `shared_chatterbox` crate (#16)
    * refactor: split `shared_chatterbox` crate
    
    * chore: format path

--- crates/backend_chatterbox_multilingual/src/utils/mod.rs
+++ crates/backend_chatterbox_multilingual/src/utils/mod.rs
@@ -10,14 +10,5 @@ pub use validate_language_id::validate_language_id;
 mod language_preparer;
 pub use language_preparer::LanguagePreparer;
 
-mod load_audio;
-pub use load_audio::load_audio;
-
-mod resample_audio;
-pub use resample_audio::resample_audio;
-
 mod inference;
 pub use inference::inference;
-
-mod repetition_penalty_logits_processor;
-pub use repetition_penalty_logits_processor::RepetitionPenaltyLogitsProcessor;
